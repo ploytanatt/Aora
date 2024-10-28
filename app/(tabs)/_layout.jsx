839081fd1a0b84e6087d7a1b253a27563beaf342
tnploy <ploythanat1@gmail.com>
2024-10-29 02:54:10 +0700

    create File & Folder Structure

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TapsLayout
+ = () => {
+  return (
+    <View>
+      <Text>TapsLayout
+      </Text>
+    </View>
+  )
+}
+
+export default TapsLayout
