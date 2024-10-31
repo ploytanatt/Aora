ee754337e58fb9ce7d337a8f542ff4bdf91311e2
tnploy <ploythanat1@gmail.com>
2024-10-31 10:24:48 +0700

    bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-300">
-      <Text className="text-3xl font-extrabold font-pblack ">Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar  style='auto' />
       <Link href={"/home"} style={{color:'blue'}}>Go to home</Link>
     </View>
